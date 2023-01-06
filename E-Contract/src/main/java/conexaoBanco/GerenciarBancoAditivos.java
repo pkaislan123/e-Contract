25673e7fd3bce82c3f0a20a82cdba9eada557c58
Aislan Silva Costa <comprasaislan@gmail.com>
2023-01-06 19:57:15 -0300

    Update GerenciarBancoAditivos.java
    teste

--- E-Contract/src/main/java/conexaoBanco/GerenciarBancoAditivos.java
+++ E-Contract/src/main/java/conexaoBanco/GerenciarBancoAditivos.java
@@ -15,7 +15,7 @@ import main.java.cadastros.CadastroAditivo;
 
 public class GerenciarBancoAditivos {
 
-	
+	//teste
 	
 	public String sql_aditivo(CadastroAditivo aditivo) {
 		return "insert into aditivo (status_aditivo, data_aditivo, texto,nome_arquivo, id_contrato_pai) values ('"
